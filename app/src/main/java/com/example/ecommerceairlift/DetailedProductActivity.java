e586e5692a77f8a7e902b34df762cb61dae0a674
Syed Raza Haider <Raza.Haider@tpsonline.com>
2021-11-12 02:44:55 +0500

    Added style@ratingBar

---
+++ app/src/main/java/com/example/ecommerceairlift/DetailedProductActivity.java
@@ -0,0 +1,14 @@
+package com.example.ecommerceairlift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailedProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detailed);
+    }
+}
